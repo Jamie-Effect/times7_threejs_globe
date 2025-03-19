e1e138cd8810b884e64dffbfebdaedce69d7247f
jamigit <jamiehbarter@gmail.com>
2025-03-20 12:22:15 +1300

    Added partner locations

--- three.js
+++ three.js
@@ -12,7 +12,6 @@ import { BokehPass } from "three/examples/jsm/postprocessing/BokehPass.js";
 
 
 
-
 //https://threejs.org/docs/index.html#manual/en/introduction/Installation
 //terminal run
 //npx vite
@@ -20,7 +19,6 @@ import { BokehPass } from "three/examples/jsm/postprocessing/BokehPass.js";
 
 // Set up the scene, camera, and renderer
       const scene = new THREE.Scene();
-  
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
       const renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
@@ -29,8 +27,6 @@ import { BokehPass } from "three/examples/jsm/postprocessing/BokehPass.js";
 
 //camera controls
 const controls = new OrbitControls( camera, renderer.domElement );
-
-
 
 camera.position.set(10,0,0);
 
@@ -63,6 +59,7 @@ function onPointerMove( event ) {
 	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
+
 }
 
 
@@ -76,14 +73,17 @@ const grey_4 = new THREE.Color( 0x7A7887 );
 const orange_1 = new THREE.Color( 0xCC4400 );
 const orange_2 = new THREE.Color( 0xDD8800 );
 
+const fog = new THREE.Color( 0x262437 );
+
 
 
 //MATERIAL -----BACKGROUND ----
 scene.background = grey_1;
+scene.fog = new THREE.Fog(fog, 3, 12);
+//scene.fog = new THREE.FogExp2( fog, 0.12 );
 
 const loader = new THREE.TextureLoader();
 //MATERIALS ------  ORBIT GLOBE--------
-    const orbitGlobeRadius = 0.2; // Radius of the smaller orbiting globes
     //const orbitGlobeMaterial = new THREE.MeshBasicMaterial({ color: orange_2, transparent: true, opacity: 0.4, side: THREE.DoubleSide  });
     const orbitGlobeMaterial = new THREE.MeshLambertMaterial({ color: orange_1, side: THREE.DoubleSide  });
 
@@ -91,6 +91,14 @@ const loader = new THREE.TextureLoader();
 //MATERIALS ------  Map glbe  --------
 const mapLocationMaterial = new THREE.MeshLambertMaterial({ color: orange_1, side: THREE.DoubleSide  });
 
+const mapLocationInteractionMaterial = new THREE.MeshStandardMaterial({ 
+  emissive: orange_1, 
+  emissiveIntensity: 8,
+  color: orange_1, 
+  transparent: true,  
+  opacity:0.1,
+side: THREE.DoubleSide  
+});
 
 
 
@@ -112,7 +120,7 @@ const mapLocationMaterial = new THREE.MeshLambertMaterial({ color: orange_1, sid
     const globeMaterial = new THREE.MeshStandardMaterial({ 
       //map: loader.load("../assets/textures/earthmap1k.jpg"),
      bumpMap: loader.load("../assets/textures/earthbump1k.jpg"),
-     bumpScale: 4,
+     bumpScale: 10,
      fog: true,
       //roughness: 0.8,
       // emissiveMap: loader.load("../assets/textures/earthspec1k.jpg"),
@@ -139,23 +147,23 @@ const mapLocationMaterial = new THREE.MeshLambertMaterial({ color: orange_1, sid
 //----------------------   OBJECTS: LIGHTS  -------------------- 
 
 //Lights
-const pl = new THREE.PointLight(0xffffff, 3000);
-pl.position.set(15, 15, 0);
+const pl = new THREE.PointLight(0xffffff, 900);
+pl.position.set(15, 6, 0);
 scene.add( pl );
 
 //Lights
-//const pl1 = new THREE.PointLight(0xffffff, 1100);
-//pl1.position.set(0, 0, -10);
-//scene.add( pl1 );
+const pl1 = new THREE.PointLight(0xffffff, 200);
+pl1.position.set(-15, 6, 0);
+scene.add( pl1 );
 
 //ambient lighting
-const al1 = new THREE.AmbientLight(0xffffff, 3);
+const al1 = new THREE.AmbientLight(0xffffff, 2);
 scene.add( al1 )
 
 
 //----------------------   OBJECTS: MESHES  --------------------
 
-// Create the earth sphere
+// Create the earth spheres. One sphere is for wireframe, other is for earth maps
 const globeRadius = 5;
 const globeRadius2 = 5.1;
 
@@ -170,69 +178,108 @@ scene.add(globe_2_wireframe);
 
 
 
-//Partner locations on sphere
-const locationGeo = new THREE.SphereGeometry(0.3, 8, 8);
-const mapLocation = new THREE.Mesh(locationGeo, mapLocationMaterial);
-scene.add(mapLocation);
+//Create spheres for partner locations 
+const locationGeo = new THREE.SphereGeometry(0.1, 8, 8);
+const locationGeoInteraction = new THREE.SphereGeometry(0.3, 8, 8);
 
 
-//Create group with main earth globe and sphere
+const partners = [];
+
+const partnerLatLong = (partnerNum,lat,long) =>{
+  // Position the smaller sphere on the surface of the main one
+  const latRad = lat * (Math.PI / 180);
+  const longRad = long * (Math.PI / 180);
+
+  // Calculate position (spherical to cartesian coordinates)
+  const x = globeRadius * Math.cos(latRad) * Math.cos(longRad);
+  const y = globeRadius * Math.sin(latRad);
+  const z = globeRadius * Math.cos(latRad) * Math.sin(longRad);
+
+  partners[partnerNum].position.set(x,y,z);
+}  
+
+
+//create all the partners and add to an array
+for(let i=0; i<=6; i++){
+  const partnerLocation = new THREE.Mesh(locationGeo, mapLocationMaterial);
+  const partnerLocationInteraction = new THREE.Mesh(locationGeoInteraction, mapLocationInteractionMaterial);
+
+  scene.add(partnerLocation);
+  scene.add(partnerLocationInteraction);
+  
+  var locationGroup = new THREE.Group();
+  scene.add( locationGroup);
+  locationGroup.add(partnerLocation);
+  locationGroup.add(partnerLocationInteraction);
+
+
+  //add partnerlocation to partners array
+  partners.push(locationGroup);
+}
+
+//add a unique location for each partner
+partnerLatLong(0,-41, 180);
+partnerLatLong(1,-30,-150);
+partnerLatLong(2,30,80);
+partnerLatLong(3,45,-130);
+partnerLatLong(4,52,5);
+partnerLatLong(5,10,0);
+
+
+//Create a group containing earth and partner locations so they can all rotate together
 var globeWithLocations = new THREE.Group();
-scene.add( globeWithLocations );
+scene.add( globeWithLocations);
+
 globeWithLocations.add(globe);
-globeWithLocations.add(mapLocation);
-
-
-const globeUV = globeGeometry.attributes.uv;
-console.log(globeUV);
+globeWithLocations.add(globe_2_wireframe);
+partners.forEach((partner) =>  globeWithLocations.add(partner));
 
 
 
-//Rotating objects
 
+//ORBIT GLOBES ------Create the rotating objects around the earth
 const globes = [];
 for(let i=0; i<8; i++){
     
-//create pivot point
+//create pivot point for circle and orbit globe to rotate together
       var pivot = new THREE.Group();
       scene.add( pivot );
 
- // Create the orbiting globe
+ //Create the orbiting globes
+  const orbitGlobeRadius = 0.1; // Radius of the smaller orbiting globes
+
     const orbitGlobeGeometry = new THREE.SphereGeometry(orbitGlobeRadius, 8, 8);
     const orbitGlobe1 = new THREE.Mesh(orbitGlobeGeometry, orbitGlobeMaterial);
     orbitGlobe1.position.set(0, 0, 0);
     scene.add(orbitGlobe1);
 
+//base radius for orbiting
+const baseOrbitRadius = 5+(i*0.35)   
 
 // Create the circle
-    const circleRadius = 7+(i*0.5);
-    const circleGeometry = new THREE.RingGeometry(circleRadius, circleRadius + 0.03, 64);
+    
+    const circleGeometry = new THREE.RingGeometry(baseOrbitRadius, baseOrbitRadius + 0.03, 64);
     const circle = new THREE.Mesh(circleGeometry, circleMaterial);
 
-    const randomRotate = Math.random (0.3,1);
+    const randomRotate = Math.random (0.5,3);
 
-    //2 rotates crazy 1 rotates in line
-    circle.rotation.x = Math.PI / 1*randomRotate; // Rotate circle to lie flat in the XY plane
+    //2 rotates crazy, 1 rotates in line
+    circle.rotation.x = Math.PI / randomRotate *1.25; // Rotate circle to lie flat in the XY plane
     scene.add(circle);
 
 
 //Add mesh to pivot group and offset position
 pivot.add( orbitGlobe1 );
-orbitGlobe1.position.set( 7+i,0, 0 ); 
-
-
+orbitGlobe1.position.set( baseOrbitRadius,0, 0 ); 
 //Add mesh to pivot group and offset position
 pivot.add( circle );
 circle.position.set(0,0, 0 ); 
-
+//add orbitglobes to circles
 circle.attach(orbitGlobe1);
-
 pivot.position.set(0,0,0);
-
 
    globes.push({
         pivot: pivot,
-      
     });
 
 }
@@ -247,8 +294,10 @@ globes.forEach((item) => {
 )
     
   
-centralPivot.add(globe);
-centralPivot.add(globe_2_wireframe);
+//centralPivot.add(globe);
+//centralPivot.add(globe_2_wireframe);
+////centralPivot.add(partnerLocation);
+centralPivot.add(globeWithLocations)
 
 const sceneGlobe = scene.children[2].children[8];
 
@@ -278,20 +327,23 @@ composer.addPass(bokehPass);
 function animate() {
   requestAnimationFrame(animate);
 
-  // Rotate the 'earth'
-  globe.rotation.y += -0.004
-  globe_2_wireframe.rotation.y += -0.004
-
-  const baseSpeed = 0.003;
-  const orbitSpeed = 0.02;
-
-  //rotate all
-  centralPivot.rotation.y += 0.003;
+  // Rotate the 'earth' and locations
+  const earthRotate = -0.007;
+  globeWithLocations.rotation.y += earthRotate;
+  
 
   
-  const rotate = (globeNum, circRotate, orbitRotate) =>{
-      globes[globeNum].pivot.rotation.z += baseSpeed * circRotate;
-      globes[globeNum].pivot.children[0].rotation.z += orbitSpeed * orbitRotate;
+  const baseCircleSpeed = 0.00220;
+  const baseOrbitSpeed = 0.02;
+
+  //rotate all items
+  centralPivot.rotation.y += 0.01;
+
+  //globenum accesses an item in the list of globes
+
+  const rotate = (globeNum, circRotateFactor, orbitRotateFactor) =>{
+      globes[globeNum].pivot.rotation.z += baseCircleSpeed * circRotateFactor;
+      globes[globeNum].pivot.children[0].rotation.z += baseOrbitSpeed * orbitRotateFactor;
     }
 
     //indvidual rotation speeds for each element in the arrary of globes
